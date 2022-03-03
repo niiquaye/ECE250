2c06abf5a0fd42152b7599ae457115fa61285414
niiquaye <omarmon@uwaterloo.ca>
2022-03-03 11:12:13 -0500

    Final Draft Project 2

--- Project-2/Resizable_deque.h
+++ Project-2/Resizable_deque.h
@@ -42,35 +42,35 @@ class Resizable_deque {
 	private:
 		// Your member variables
 
-        int m_front {};
-		int m_back {};
-        int m_head_index {};
-		std::size_t m_size {};
-	    //std::size_t m_initCap{};
-		std::size_t m_cap {};
-        std::size_t m_initCap {};
-		// Any private member functions
-		//   - helper functions for resizing your array?
-        Type* deque;
-        void double_capacity();
-        void half_capacity();
+        int m_front {};            // head pointer
+		int m_back {};             // tail pointer
+        int m_head_index {};       // head index used to compute head pointer
 
-        void print();
+		std::size_t m_size {};     // size of deque
+		std::size_t m_cap {};      // capacity of deque
+        std::size_t m_initCap {};  // initial capacity of deque
+
+        Type* deque;               // deque pointer
+
+
+		// Any private member functions
+        void double_capacity();   // doubles deque capacity
+        void half_capacity();     // halves deque capacity
+
 
 
         
 	// Friends
 
+    // std::cout operator overload
 	template <typename T>
 	friend std::ostream &operator<<( std::ostream &, Resizable_deque<T> const & );
 
+    // absolute value function
     template<typename T>
     friend T abs(T a); 
 };
-/***
- * elements [i] = old[(i+front)% old_capacity]
- *
- **/ 
+ 
 
 /////////////////////////////////////////////////////////////////////////
 //                   Constructors and Destructors                      //
@@ -111,7 +111,7 @@ Resizable_deque<Type>::Resizable_deque( Resizable_deque &&deque )
 // Destructor
 template <typename Type>
 Resizable_deque<Type>::~Resizable_deque() {
-	// Enter your implementation here
+	// free allocated memory
     if(deque) delete [] deque;
 }
 
@@ -121,25 +121,27 @@ Resizable_deque<Type>::~Resizable_deque() {
 
 template <typename Type>
 int Resizable_deque<Type>::size() const {
-	// Enter your implementation here
+	// return current size of deque  
 	return static_cast<int>(this->m_size);
 }
 
 template <typename Type>
 int Resizable_deque<Type>::capacity() const {
-	// Enter your implementation here
+	// return current capacity of deque
 	return static_cast<int>(this->m_cap);
 }
 
 template <typename Type>
 bool Resizable_deque<Type>::empty() const {
-	// Enter your implementation here
+	// return true if size equals zero
 	return (this->m_size == 0);
 }
 
 template <typename  Type>
 Type Resizable_deque<Type>::front() const {
-	// Enter your implementation here
+	// check if deque is not empty and ensure m_front ( aka deque head)
+    // is non-negative value then return the front of the deque
+    // else throw and underflow
     if(!this->empty() && m_front >= 0)
     	return deque[m_front];
     else 
@@ -148,11 +150,12 @@ Type Resizable_deque<Type>::front() const {
 
 template <typename  Type>
 Type Resizable_deque<Type>::back() const {
-	// Enter your implementation here
+	// check if deque is not empty and ensure m_back  ( aka deque tail)
+    // is non-negative value then return the back  of the deque
+    // else throw and underflow
     if(!this->empty() && m_back >= 0)
 	   return deque[m_back];
     else{
-        //std::cout << "hello" << std::endl;
         throw underflow();
 
     }
@@ -161,7 +164,6 @@ Type Resizable_deque<Type>::back() const {
 template <typename Type>
 void Resizable_deque<Type>::swap( Resizable_deque<Type> &deque ) {
 	// Swap the member variables
-	//     std::swap( variable, deque.variable );
     std::swap(this->m_size, deque.m_size);
     std::swap(this->m_front, deque.m_front);
     std::swap(this->m_cap, deque.m_cap);
@@ -169,7 +171,6 @@ void Resizable_deque<Type>::swap( Resizable_deque<Type> &deque ) {
 
     std::swap(this->deque, deque.deque);
 
-	// Enter your implementation here
 }
 
 template <typename Type>
@@ -191,92 +192,112 @@ Resizable_deque<Type> &Resizable_deque<Type>::operator=( Resizable_deque<Type> &
 
 template <typename Type>
 void Resizable_deque<Type>::push_front( Type const &obj ) {
-	// Enter your implementation here
+	// check if array is full, if so double capacity 
     if(this->m_size == this->m_cap)
         /*array needs to be doubled*/
         this->double_capacity();
 
-    //this->m_head_index = (this->m_size == 0)? 0: this->m_head_index--;
+    // if size is equal to zero reset head index counter to 0
+    // if not decrement the head index counter
     if(m_size == 0)
         m_head_index = 0;
     else 
         m_head_index--;
-    //std::cout << "head index: " << m_head_index << std::endl;
-    //this->m_front--;
+
+    // calculate the head pointer location by taking the head index counter mod the capacity
+    // head index counter will be negative thus the mod of a negative number is as follows
+    // -n % k = k - n % k ; where n is head index counter and k is capacity
     this->m_front = (this->m_size == 0)? 0 : this->m_cap - (abs(this->m_head_index) % this->m_cap) ;
+
+    // add element to the deque
     this->deque[this->m_front] = obj;
     
-
+    // augment the size by 1
     this->m_size++;
-    //std::cout << "front: " << m_front <<" "<< "back: " << m_back <<" "<< "size: " << m_size << std::endl;
 }
 
 template <typename Type>
 void Resizable_deque<Type>::push_back( Type const &obj ) {
-	// Enter your implementation here
+	// check if array is full, is so double capacity
     if(this->m_size == this->m_cap)
         /*array needs to be doubled*/
         this->double_capacity();
     
+    // increment tail pointer, if size is 0 ensure the tail pointer is 0,
+    // else mod the location of the back pointer by the capacity
     this->m_back++;
     this->m_back = (this->m_size == 0)? 0 : this->m_back % this->m_cap;
+
+    // add element to the deque
     this->deque[this->m_back] = obj;
 
-
+    // augment the size by 1
     this->m_size++;
 
-    //std::cout << "front: " << m_front <<" "<< "back: " << m_back <<" "<< "size: " << m_size << std::endl;
 }
 
 template <typename Type>
 void Resizable_deque<Type>::pop_front() {
-	// Enter your implementation here
-    
+	// check if deque is empty if so throw underflow
     if(this->empty()) throw underflow{};
 
-    this->m_head_index++; 
-    //this->m_front = (this->m_front == this->m_cap-1)? 0 : this->m_front++;
+    // increase head index to keep track of where 
+    // the new head to be calculated will be
+    this->m_head_index++;
+
+    // pop of element from deque head by incrementing the
+    // head pointer, if head pointer equal to capacity -1 
+    // wrap back around to 0
     if(m_front == m_cap - 1)
         m_front = 0;
     else 
         m_front++;
+
+    // decrease the size of the deque
     this->m_size--;
 
+    // check if deque capacity needs to be halved
     if(this->m_size <= this->m_cap/4 && this->m_cap/2 >= this->m_initCap)
-      /*half the array*/
+      /*half the deque capacity*/
       this->half_capacity();
     
-    //std::cout << "front: " << m_front <<" "<< "back: " << m_back <<" "<< "size: " << m_size << std::endl;
 
 }
 
 template <typename Type>
 void Resizable_deque<Type>::pop_back() {
-	// Enter your implementation here
+	// check if deque is empty if so throw underflow
     if(this->empty()) throw underflow{};
 
     
-    //this->m_back = (this->m_back == 0 )? this->m_cap - 1  : this->m_back--;
+    // pop of element from deque tail by decrementing the
+    // tail pointer, if tail pointer equal to 0 wrap back         
+    // around to capacity -1 
     if(m_back == 0)
         m_back = m_cap - 1;
     else
         m_back--;
+
+    // decrease the size of the deque
     this->m_size--;
 
-    //std::cout << "new tail: "<< m_back << std::endl;
+    // check if the deque needs to be halved
     if(this->m_size <= this->m_cap/4 && this->m_cap/2 >= this->m_initCap)
-      /*half the array*/
+      /*half the deque capacity*/
       this->half_capacity();
 
 }
 
 template <typename Type>
 void Resizable_deque<Type>::clear() {
-	// Enter your implementation here
+	// reset all deque members to 0
     this->m_size = 0;
     this->m_back = 0;
     this->m_front = 0;
+    this->m_head_index = 0;
 
+    // resize deque to intial capacity if it is not
+    // equal to initial capacity
     if(this->m_initCap != this->m_cap){
       Type* deletion_ptr = this->deque;
       delete [] deletion_ptr;
@@ -293,52 +314,61 @@ void Resizable_deque<Type>::clear() {
 
 template<typename Type>
 void Resizable_deque<Type>::double_capacity(){
-    
-    std::cout << "double cap" << std::endl;
-
     // allocate new array
     Type* new_array = new Type[this->m_cap*2];
-    // elements [i] = old[(i+front)% old_capacity]
 
+    // copy data from old array into newly allocated array
     for(std::size_t i{0}; i < this->m_size; i++){
         new_array[i] = deque[(this->m_front)%this->m_cap];
         this->m_front++;
     }
 
+    // deallocate old deque
     Type* deletion_ptr = this->deque;
     delete [] deletion_ptr;
     deletion_ptr = nullptr;
 
+    // set the deque pointing to the newly allocated array
     this->deque = new_array;
+
+    // double the capacity
     this->m_cap *= 2;
 
+    // set tail pointer to size - 1
     this->m_back = this->m_size - 1;
+
+    // set head pointer and head index to 0
     this->m_front = 0; this->m_head_index = 0;
      
 }
 
 template<typename Type>
 void Resizable_deque<Type>::half_capacity(){
-
-    std::cout << "half cap" << std::endl;
-
     // allocate new array
     Type* new_array = new Type[this->m_cap/2];
 
+    // copy data from old array into newly allocated array
     for(std::size_t i{0}; i < this->m_size; i++){
         new_array[i] = deque[(this->m_front)%this->m_cap];
         this->m_front++;
     }
 
+    // dealloacate old deque
     Type* deletion_ptr = this->deque;
     delete [] deletion_ptr;
     deletion_ptr = nullptr;
 
+    // set the deque pointing to the newly allocated array
     this->deque = new_array;
+
+    // half the capacity
     this->m_cap /= 2;
 
+    // set tail pointer to size -1
     this->m_back = this->m_size - 1;
-    this->m_front = 0;
+
+    // set head pointer to 0 and head index to 0
+    this->m_front = 0; this->m_head_index = 0;
 
 }
 
@@ -346,6 +376,7 @@ void Resizable_deque<Type>::half_capacity(){
 //                               Friends                               //
 /////////////////////////////////////////////////////////////////////////
 
+// friend that computes the magnitude of a given value
 template <typename T>
 T abs (T a) {return (a < 0)? a*-1: a;} 
 
@@ -354,15 +385,11 @@ T abs (T a) {return (a < 0)? a*-1: a;}
 template <typename T>
 std::ostream &operator<<( std::ostream &out, Resizable_deque<T> const &list ) {
 
-   // for(auto i = 0; (list.m_front+i)%list.m_cap != list.m_back; ++i){
-   //     
-   //     out << list.deque[(list.m_front + i) % list.m_cap] << " ";
-   // 
-   // }
-
-    for(auto i = 0; i < list.m_cap; i++)
-        out << list.deque[i] << " ";
-
+    for(auto i = 0; (list.m_front+i)%list.m_cap != list.m_back; ++i){
+        
+        out << list.deque[(list.m_front + i) % list.m_cap] << " ";
+    
+    }
 
 	return out;
 }
